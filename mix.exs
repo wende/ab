960f2204cfd9c5de856fe04ce8bcea3835431e5b
wende <wende@hey.com>
2025-10-03 14:21:59 +0200

    Lower minimum Elixir version to 1.15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AB.MixProject do
     [
       app: :ab,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
